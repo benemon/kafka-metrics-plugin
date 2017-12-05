a5d5863e321cd14f46d84fcef21a03f46fde8f31
Benjamin Holmes <benjamin.holmes@gmail.com>
2017-12-05 14:50:31 +0000

    added kafkaMetricsPublisher build step

--- src/test/java/com/redhat/ukiservices/bholmes/jenkins/producer/MessageProducerTest.java
+++ src/test/java/com/redhat/ukiservices/bholmes/jenkins/producer/MessageProducerTest.java
@@ -2,6 +2,7 @@ package com.redhat.ukiservices.bholmes.jenkins.producer;
 
 import info.batey.kafka.unit.KafkaUnitRule;
 import net.sf.json.JSONObject;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MessageProducerTest {
 
+    public static final String PRODUCER_TEST_TOPIC = "producer-test";
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
@@ -25,6 +27,7 @@ public class MessageProducerTest {
     @Before
     public void before() throws Exception {
         producer = new MessageProducer();
+        kafkaRule.getKafkaUnit().createTopic(PRODUCER_TEST_TOPIC);
     }
 
     @Test
@@ -33,11 +36,16 @@ public class MessageProducerTest {
         JSONObject payload = new JSONObject();
         payload.put("message", "success!");
 
-        producer.sendMessage("unit-test", payload.toString());
+        producer.sendMessage(PRODUCER_TEST_TOPIC, payload.toString());
 
-        List<String> messages = kafkaRule.getKafkaUnit().readMessages("unit-test", 1);
+        List<String> messages = kafkaRule.getKafkaUnit().readMessages(PRODUCER_TEST_TOPIC, 1);
         assertEquals(messages.size(), 1);
         JSONObject receivedPayload = JSONObject.fromObject(messages.get(0));
         assertEquals(receivedPayload.getString("message"), "success!");
+    }
+
+    @After
+    public void after() throws Exception {
+        kafkaRule.getKafkaUnit().deleteTopic(PRODUCER_TEST_TOPIC);
     }
 }
